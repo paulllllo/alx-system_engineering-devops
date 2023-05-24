5830af483127fbfa53ffa84d7278ad29a86667a7
Kelechi <paulkelechi11@gmail.com>
2023-05-24 02:56:56 -0700

    [Correction]

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+retrieves info from an api and prints the values to the standard Output
+imports are arranged in alphabetical order
+"""
 import requests
 import sys
 
 ID = sys.argv[1]
-user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}')
-user = user_response.json()
-todo_response = requests.get('https://jsonplaceholder.typicode.com/todos')
-todos = todo_response.json()
+user_res = requests.get(f'https://jsonplaceholder.typicode.com/users/{ID}')
+user = user_res.json()
+todo_res = requests.get('https://jsonplaceholder.typicode.com/todos')
+todos = todo_res.json()
 
 total_tasks = 0
 done_tasks = []
@@ -18,6 +22,7 @@ if __name__ == "__main__":
             if todo["completed"]:
                 done_tasks.append(todo["title"])
 
-                print(f'Employee {user["name"]} is done with tasks({len(done_tasks)}/{total_tasks}):')
+                print(f'Employee {user["name"]} is done with tasks\
+({len(done_tasks)}/{total_tasks}):')
                 for task in done_tasks:
                     print(f"\t {task}")
