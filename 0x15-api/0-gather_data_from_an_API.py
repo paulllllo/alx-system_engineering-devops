440480871c54064d274ab90996440c88ab14645c
Kelechi <paulkelechi11@gmail.com>
2023-05-24 02:49:06 -0700

    [Correction]

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,12 +11,13 @@ todos = todo_response.json()
 total_tasks = 0
 done_tasks = []
 
-for todo in todos:
-    if str(todo["userId"]) == ID:
-        total_tasks += 1
-        if todo["completed"]:
-            done_tasks.append(todo["title"])
+if __name__ == "__main__":
+    for todo in todos:
+        if str(todo["userId"]) == ID:
+            total_tasks += 1
+            if todo["completed"]:
+                done_tasks.append(todo["title"])
 
-print(f'Employee {user["name"]} is done with tasks({len(done_tasks)}/{total_tasks}):')
-for task in done_tasks:
-    print(f"\t {task}")
+                print(f'Employee {user["name"]} is done with tasks({len(done_tasks)}/{total_tasks}):')
+                for task in done_tasks:
+                    print(f"\t {task}")
