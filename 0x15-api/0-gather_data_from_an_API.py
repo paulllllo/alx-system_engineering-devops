53240222d00a8cf3233f67661a7ccdd18b5eb53e
Kelechi <paulkelechi11@gmail.com>
2023-05-24 10:22:39 -0700

    [Correction]

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(ID)
     user_res = requests.get(url)
     user = user_res.json()
+    employeeName = user.get("name")
+    
 
     todoUrl = url + "/todos"
     response = requests.get(todoUrl)
@@ -23,7 +25,8 @@ if __name__ == "__main__":
         if task.get('completed'):
             done_tasks.append(task.get("title"))
 
-    print('Employee {} is done with tasks\
-({}/{}):'.format(user.get("name"), len(done_tasks), len(tasks)))
+    print("Employee {} is done with tasks({}/{}):"
+        .format(employeeName, len(done_tasks), len(tasks)))
+
     for task in done_tasks:
         print("\t {}".format(task))
